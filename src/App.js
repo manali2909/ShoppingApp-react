baf052553241048833609c4825eaf60cbde04581
June <june.qualwebs@gmail.com>
2021-02-26 21:26:23 +0530

    url home chenged-undo

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
             <div>
                 <Navbar totalItems={cart.total_items}/>
                 <Switch>
-                    <Route path='/ShoppingApp-react'>
+                    <Route path='/'>
                         <Products products={products} onAddToCart={handleAddToCart}/>
                     </Route>
                     <Route exact path ='/cart'>
